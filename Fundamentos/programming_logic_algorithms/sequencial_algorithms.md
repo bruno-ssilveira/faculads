e1179a109be554e63d17d7ad053de2927c7aa5d0
bruno <bruno.santossilveira001@gmail.com>
2025-05-17 20:55:12 -0300

    ok

---
+++ Fundamentos/programming_logic_algorithms/sequencial_algorithms.md
@@ -0,0 +1,5 @@
+## Variáveis, dados e seus tipos
+
+- Conjunto oficial de regras e boas práticas do Python: https://peps.python.org/pep-0008/
+
+- Python tem como boa prática no nome de variáveis utilizar underline invés de camelcase "preco_total"...
--- Fundamentos/programming_logic_algorithms/sequencial_algorithms.py
+++ Fundamentos/programming_logic_algorithms/sequencial_algorithms.py
@@ -1,3 +1,22 @@
+#idade = input('Qual sua idade? ')
+
 print('teste')
 print(2+3)
-print("olá", 'mundo')
+print("olá",'mundo') #comment
+
+teste = 'teste'
+numero = 123
+
+s1 = f'testando: {teste}, {numero}'
+  
+print(s1)
+
+# "len" para pegar tamanho
+print(len(s1))
+
+# fnc em python
+def printar(toprint):
+    print(toprint)
+    print(1+1)
+        
+printar('teste fnc toprint')
